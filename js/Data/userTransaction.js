08b10e38bf2cab00a74d30746dc0bcc8f8a8a47a
AryanSherigar <aryansherigar@gmail.com>
2024-06-09 20:14:52 +0530

    Usertransactiohistory commented

--- js/Data/userTransaction.js
+++ js/Data/userTransaction.js
@@ -20,78 +20,78 @@ function generateCountDown(timestamp) {
 const contractTransactionList = document.querySelector(".dataUserTransaction");
 const UserProfile = document.querySelector(".contract-user");
 
-const userTransactionHistory = userTransaction.map((transaction, i) => {
-  return `
-    <div class="col-12 col-md-6 col-lg-4 item explore-item" data-groups='["ongoing","ended"]'>
-    <div class="card project-card">
-      <div class="media">
-        <a href="project-details.html">
-          <img src="assets/img/content/thumb_${i + 1}.png" alt="" class="card-img-top avatar-max-ig"/>
-        </a>
+// const userTransactionHistory = userTransaction.map((transaction, i) => {
+//   return `
+//     <div class="col-12 col-md-6 col-lg-4 item explore-item" data-groups='["ongoing","ended"]'>
+//     <div class="card project-card">
+//       <div class="media">
+//         <a href="project-details.html">
+//           <img src="assets/img/content/thumb_${i + 1}.png" alt="" class="card-img-top avatar-max-ig"/>
+//         </a>
 
-        <div class="media-body ml-4">
-          <a class="project-details.html">
-            <h4 class="m-0">#tbCoders</h4>
-          </a>
-          <div class="countdown-times">
-            <h6 class="my-2">Transaction NO: ${i + 1}</h6>
+//         <div class="media-body ml-4">
+//           <a class="project-details.html">
+//             <h4 class="m-0">#tbCoders</h4>
+//           </a>
+//           <div class="countdown-times">
+//             <h6 class="my-2">Transaction NO: ${i + 1}</h6>
 
-            <div class="countdown d-flex" data-data="2022-06-30"></div>
-          </div>
-      </div>
-    </div>
+//             <div class="countdown d-flex" data-data="2022-06-30"></div>
+//           </div>
+//       </div>
+//     </div>
 
-    <div class="card-body">
-      <div class="items">
-        <div class="single-item">
-          <span>
-          ${transaction.token / 10 ** 18 ? "Amount" : "ClaimToken"}  
-          </span>
-          <span>${transaction.token / 10 ** 18 || ""}</span>
-        </div>
-        <div class="single-item">
-          <span>${transaction.gas}</span>
-          <span></span>
-        </div>
-        <div class="single-item">
-          <span>Status</span>
-          <span>${transaction.status}</span>
-        </div>
-      </div>
-    </div>
+//     <div class="card-body">
+//       <div class="items">
+//         <div class="single-item">
+//           <span>
+//           ${transaction.token / 10 ** 18 ? "Amount" : "ClaimToken"}  
+//           </span>
+//           <span>${transaction.token / 10 ** 18 || ""}</span>
+//         </div>
+//         <div class="single-item">
+//           <span>${transaction.gas}</span>
+//           <span></span>
+//         </div>
+//         <div class="single-item">
+//           <span>Status</span>
+//           <span>${transaction.status}</span>
+//         </div>
+//       </div>
+//     </div>
 
-    <div class="project-footer d-flex align-item-center">
-      <a class="btn btn-bordered-white btn-smaller" href="https://amoy.polygonscan.com/tx/${transaction.transactionHash}">Transaction</a>
+//     <div class="project-footer d-flex align-item-center">
+//       <a class="btn btn-bordered-white btn-smaller" href="https://amoy.polygonscan.com/tx/${transaction.transactionHash}">Transaction</a>
 
-      <div class="social-share ml-auto">
-        <ul class="d-flex list-unstyled">
-          <li>
-            <a href="#">
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-telegram"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-globe"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-discord"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+//       <div class="social-share ml-auto">
+//         <ul class="d-flex list-unstyled">
+//           <li>
+//             <a href="#">
+//               <i class="fab fa-twitter"></i>
+//             </a>
+//             <a href="#">
+//               <i class="fab fa-telegram"></i>
+//             </a>
+//             <a href="#">
+//               <i class="fab fa-globe"></i>
+//             </a>
+//             <a href="#">
+//               <i class="fab fa-discord"></i>
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
 
-    <div class="blockchain-icon">
-      <img src="assets/img/content/ethereum.png" alt="">
-    </div>
-  </div>
+//     <div class="blockchain-icon">
+//       <img src="assets/img/content/ethereum.png" alt="">
+//     </div>
+//   </div>
     
-    `
-})
+//     `
+// })
 
-contractTransactionList.innerHTML = userTransactionHistory;
+// contractTransactionList.innerHTML = userTransactionHistory;
 
 const UserProfileHtml = `
 <div class="contract-user-profile">
